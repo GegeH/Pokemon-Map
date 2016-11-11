ab1312bc2221674bb5667c45be372ac8489b0605
Gege <gegehuang.gh@gmail.com>
2016-11-10 21:34:47 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -4,3 +4,11 @@ function loadMapScenario() {
         credentials: 'Ahx-pu4sUrSfP72S4B7gtzvf_FB6JV5OTgJazEna5ZQ7QFm67ATaqUI3K-KSLtxz'
     });
 }
+
+// 1. Define pokemon data format, create mock pokemon data
+
+// 2. Create pokemon image on map
+
+// 3. Add pokemon counter down refresh
+
+// 4. Connect with REST API
