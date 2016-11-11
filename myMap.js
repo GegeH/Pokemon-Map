587525166e6621ed897e2f472036d2a9fdbdf7bd
Gege <gegehuang.gh@gmail.com>
2016-11-11 02:36:23 -0500

    try add a mircosoft layer on bing map

--- myMap.js
+++ myMap.js
@@ -6,9 +6,28 @@ function loadMapScenario() {
 }
 
 // 1. Define pokemon data format, create mock pokemon data
+map_items = [
+    {
+        "pokemon_id" : 12,
+        "expire" : 1234567,
+        "longitude" : -73.9828028,
+        "latitude" : 40.7575988
+    }
+]
 
 // 2. Create pokemon image on map
+function get_pokemon_layer_from_map_itmes(map_items) {
+    var pushpins = Microsoft.Maps.TestDataGenerator.getPushpins(10, map.getBounds());
+    var layer = new Microsoft.Maps.Layer();
+    return layer;
+}
+    
 
+var pokemon_layer = get_pokemon_layer_from_map_itmes(map_items)
+layer.add(pushpins);
+map.layers.insert(layer);
+    
+    
 // 3. Add pokemon counter down refresh
 
 // 4. Connect with REST API
