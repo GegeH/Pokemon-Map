fba939fcab53a8b1a06d6a65061c258e783c4554
GegeH <gegehuang.gh@gmail.com>
2016-11-24 21:46:21 +0000

    update

---
+++ pokemon_map/map/views.py
@@ -0,0 +1,12 @@
+import logging
+import json
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def pokemons(request):
+    # 1. Get longtude latitude info
+
+    # 2. Query database
+
+    return HttpResponse("Pokemon-Map!")
