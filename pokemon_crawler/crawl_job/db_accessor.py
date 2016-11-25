8695f564675f657aea981c362e0f3c3eca4541a7
GegeH <gegehuang.gh@gmail.com>
2016-11-25 01:26:16 +0000

    updata

--- pokemon_crawler/crawl_job/db_accessor.py
+++ pokemon_crawler/crawl_job/db_accessor.py
@@ -11,10 +11,9 @@ def add_pokemon_to_db(encounter_id, expire, pokemon_id, latitude, longitude):
 	# 2. Execute SQL
 	with conn.cursor() as cur:
 		cur.execute("INSERT INTO pokemon_map (encounter_id, expire, pokemon_id, latitude, longitude)" + 
-					"VALUES (%s, %s, %s, %s, %s)" +
-					"ON CONFLICT (encounter_id) DO NOTHING", (encounter_id, expire, pokemon_id, latitude, longitude))
+					" VALUES (%s, %s, %s, %s, %s)" +
+					" ON CONFLICT (encounter_id) DO NOTHING", (encounter_id, expire, pokemon_id, latitude, longitude))
 
 	# 3. Connection  commit
 	conn.commit()
-
 	return
--- pokemon_map/map/views.py
+++ pokemon_map/map/views.py
@@ -4,9 +4,17 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from db_accessor import get_pokemons_from_db
+
+
 def pokemons(request):
     # 1. Get longtude latitude info
+    north = request.GET["north"]
+    south = request.GET["south"]
+    west = request.GET["west"]
+    east = request.GET["east"]
 
     # 2. Query database
+    result = get_pokemons_from_db(north, south, west, east)
+    return HttpResponse(json.dumps(result))
 
-    return HttpResponse("Pokemon-Map!")
