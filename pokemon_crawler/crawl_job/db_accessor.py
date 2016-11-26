28d62bd075349e9586564f87af048b1c71a0816d
GegeH <gegehuang.gh@gmail.com>
2016-11-26 23:10:17 +0000

    eb deploy success

--- pokemon_crawler/crawl_job/db_accessor.py
+++ pokemon_crawler/crawl_job/db_accessor.py
@@ -9,11 +9,11 @@ def add_pokemon_to_db(encounter_id, expire, pokemon_id, latitude, longitude):
 							database = "pokemondb")
 
 	# 2. Execute SQL
-	with conn.cursor() as cur:
+    with conn.cursor() as cur:
 		cur.execute("INSERT INTO pokemon_map (encounter_id, expire, pokemon_id, latitude, longitude)" + 
 					" VALUES (%s, %s, %s, %s, %s)" +
 					" ON CONFLICT (encounter_id) DO NOTHING", (encounter_id, expire, pokemon_id, latitude, longitude))
 
 	# 3. Connection  commit
-	conn.commit()
-	return
+    conn.commit()
+    return
