8fe86ab5aba216f41169ea44a0ea18e490d73c56
GegeH <gegehuang.gh@gmail.com>
2016-12-19 06:18:41 +0000

    before web server eb

--- pokemon_map/map/views.py
+++ pokemon_map/map/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+import logging
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def pokemons(request):
+    return HttpResponse("Charmander")
