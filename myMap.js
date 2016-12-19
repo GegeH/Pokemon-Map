a61c103cba826da714ea12deab941d93d9e39e82
Gege <gegehuang.gh@gmail.com>
2016-12-18 21:47:56 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -17,6 +17,7 @@ function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AjHg4poymQ-3nnoVKmwS7U3gSkfZLFWu8pah1HIa0N8tv2gzaJoZvqHBAcWNADb7'
     });
+    add_pokemon_layer();
 }
 
 // 1. Define pokemon data format, create mock pokemon data
