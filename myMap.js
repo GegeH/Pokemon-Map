646a20d456c411ecef405d33bb4a529f3bd9963e
Gege <gegehuang.gh@gmail.com>
2016-12-18 21:29:54 -0500

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AjHg4poymQ-3nnoVKmwS7U3gSkfZLFWu8pah1HIa0N8tv2gzaJoZvqHBAcWNADb7'
+    });
+}
